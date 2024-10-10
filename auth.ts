f2cf1da6c2a7c980d34e26a35d616599f9f5cebc
Amrit Rai <iamamrit27@gmail.com>
2024-10-10 20:52:22 +0530

    add drizzle

--- auth.ts
+++ auth.ts
@@ -1,9 +1,14 @@
 import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/prisma"
-import google from "next-auth/providers/google"
- 
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
+import { db } from "@/db/index"
+import GoogleProvider from "next-auth/providers/google"
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [google],
+  adapter: DrizzleAdapter(db),  
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
 })
