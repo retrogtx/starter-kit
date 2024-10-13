1e0238c8ada30aece551bc1dcdb93de023fdba90
Amrit Rai <iamamrit27@gmail.com>
2024-10-13 10:38:19 +0530

    update title, add subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -19,15 +19,19 @@ export default function Home() {
           )}
         >
           <AnimatedShinyText className="inline-flex items-center justify-center px-3 py-0.5 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
-            <span>✨ Join our waitlist</span>
+            <span>✨ Introducing Starter</span>
             <ArrowRightIcon className="ml-1 size-2.5 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
           </AnimatedShinyText>
         </div>
-        <div className="text-center text-4xl font-bold">
-          <span className="bg-gradient-to-b from-white to-gray-400 text-transparent bg-clip-text">
-            Accelerate your Indie Hacking Journey,<br/>
-            By Amrit.
-          </span>
+        <div className="text-center max-w-4xl mx-auto z-10">
+
+          <h1 className="text-5xl md:text-7xl font-bold mb-6">
+            Accelerate your Indie Journey with this Starter.
+          </h1>
+
+          <p className="text-xl mb-8 text-gray-200">
+            Designed to help you launch your next project faster by <a href="https://x.com/amritwt" target="_blank" rel="noopener noreferrer" className="underline hover:text-blue-300 transition-colors">@amritwt</a> ❤️
+          </p>
         </div>
         <div className="flex flex-col items-center justify-center space-y-4">
           <EmailWaitlist />
