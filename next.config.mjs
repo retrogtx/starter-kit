3e414029932631ff0f8dc5defc48ec2103ee923c
Amrit Rai <iamamrit27@gmail.com>
2024-10-16 09:11:33 +0530

    fix errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'assets.aceternity.com'],
+    domains: ['images.unsplash.com', 'assets.aceternity.com', 'avatars.githubusercontent.com'],
   },
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
