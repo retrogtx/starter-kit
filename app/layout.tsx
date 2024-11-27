8c887fafda1b91ac112664cb426d924d8e048921
Amrit <iamamrit27@gmail.com>
2024-11-27 16:27:31 +0530

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,20 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from "@vercel/analytics/react"
+
 export const metadata: Metadata = {
+  metadataBase: new URL("https://starter-mocha-five.vercel.app"),
   title: "Starter Kit",
   description: "Starter Kit for your Indie Hacking Journey.",
+  keywords: ["Indie Hacking", "Starter Kit", "Indie Maker", "Indie Hackers", "Ship Fast", "Indie"],
+  openGraph: {
+    title: "Starter Kit",
+    description: "Starter Kit for your Indie Hacking Journey.",
+    url: "https://starter-mocha-five.vercel.app",
+    siteName: "Starter Kit",
+    locale: "en_US",
+    type: "website",
+  }
 };
 
 export default function RootLayout({
